fae5fb4730b5dc0499648f5c7466333467cd2671
Radić Ivan <0135279367@FPZ.HR>
2024-11-05 11:23:54 +0100

    Dodavanje funkcije za provjeru broja unutar raspona [10, 100]

--- vjezbe 5 git.py
+++ vjezbe 5 git.py
@@ -1,1 +1,11 @@
-print("Dobrodošli na Računalstvo u prometnom inženjerstvu")
+def provjera_broja(broj):
+    if 10 <= broj <=100:
+        return f"Broj {broj} je unutar raspona."
+    else:
+        return f"Broj {broj}je izvan raspona."
+if __name__ == "__main__":
+    try:
+        uneseni_broj = int (input("unesite broj: "))
+        print(provjera_broja(uneseni_broj))
+    except ValueError:
+        print("unesena vrijednost nije broj.")
